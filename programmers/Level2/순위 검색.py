2832a9b1bfa42751a9ce423bcc70ef07baa98c02
byeongjaeSon <byeongjae.dev@gmail.com>
2022-08-29 15:12:16 +0900

    feat: Add the solution to '72412-순위 검색' on Programmers

---
+++ programmers/Level2/순위 검색.py
@@ -0,0 +1,29 @@
+# 프로그래머스 - 순위 검색(72412)
+# https://school.programmers.co.kr/learn/courses/30/lessons/72412
+
+from collections import defaultdict
+import bisect
+
+def solution(info, query):
+    answer = []
+    condition_to_scores = defaultdict(list)
+    
+    for e in info:
+        split_info = e.split(' ')
+        condition_to_scores[tuple(split_info[:4])].append(int(split_info[4]))
+    
+    for scores in condition_to_scores.values():
+        scores.sort()
+    
+    for q in query:
+        split_q = q.split(' ')
+        threshold = int(split_q[7])
+        q_condition = [split_q[i] for i in range(0, 7, 2) if split_q[i] != '-']
+        
+        cnt = 0
+        for condition, scores in condition_to_scores.items():
+            if all(map(lambda c : c in condition, q_condition)):
+                cnt += len(scores) - bisect.bisect_left(scores, threshold)
+        answer.append(cnt)
+        
+    return answer
