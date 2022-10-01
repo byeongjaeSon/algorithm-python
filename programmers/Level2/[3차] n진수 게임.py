f84f1caaa4841857e5433fbabaaa560328e01fa4
손병재 <byeongjae.dev@gmail.com>
2022-10-01 15:54:21 +0900

    Feat: Add the solution to '17687-[3차] n진수 게임' on Programmers

---
+++ programmers/Level2/[3차] n진수 게임.py
@@ -0,0 +1,19 @@
+import string
+
+number = string.digits + string.ascii_uppercase
+
+def convert_recur(num, base):
+    q, r = divmod(num, base)
+    return convert_recur(q, base) + number[r] if q else number[r]
+    
+
+def solution(n, t, m, p):
+    seq = ""
+    num = 0
+    while len(seq) <= t * m:
+        seq += convert_recur(num, n)
+        num += 1
+    
+    ans_list = [seq[(m * i + p) - 1] for i in range(t)]
+    
+    return ''.join(ans_list)
