3c3c0e5d43e1a52ce6a7cfb8a29a640c1d3a4cbc
손병재 <byeongjae.dev@gmail.com>
2022-10-02 09:58:59 +0900

    12952-N-Queen

---
+++ programmers/Level2/N-Queen.py
@@ -0,0 +1,21 @@
+def put_queen(pos, n):
+    if len(pos) == n:
+        return 1
+    
+    y = 0 
+    if len(pos) != 0:
+        y = pos[-1][0] + 1
+    
+    ret = 0
+    for x in range(n):
+        for p in pos:
+            if (p[1] == x) or (y - p[0] == abs(p[1] - x)):
+                break
+        else:
+            pos.append((y,x))
+            ret += put_queen(pos, n)
+            pos.pop()
+    return ret
+                
+def solution(n):
+    return put_queen([], n)
