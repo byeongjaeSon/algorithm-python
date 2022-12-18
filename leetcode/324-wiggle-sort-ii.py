2f099789ce008f4ff523a06f1427cfa541144478
손병재 <byeongjae.dev@gmail.com>
2022-12-18 21:19:56 +0900

    Refactor: Modify the solution to '324-wiggle-sort-ii' on LeetCode

--- leetcode/324-wiggle-sort-ii.py
+++ leetcode/324-wiggle-sort-ii.py
@@ -1,18 +1,33 @@
 class Solution:
-    def wiggleSort(self, nums: List[int]) -> None:
-        nums.sort()
-        temp = []
-        mid = (len(nums)-1)//2
-        l, r = mid, len(nums)-1
-        
-        while l >= 0 and r > mid:
-            temp.append(nums[l])
-            temp.append(nums[r])
-            l -= 1
-            r -= 1
-        if l == 0:
-            temp.append(nums[l])
+    def wiggleSort(self, nums: List[int]) -> None:        
+        num_to_cnt = [0 for _ in range(5001)]
+        acc_cnt, mid_cnt = 0, ceil(len(nums)/2)
+        mid_key = 0
+        for num in nums:
+            num_to_cnt[num] += 1
 
-        for i in range(len(temp)):
-            nums[i] = temp[i]
+        for num, cnt in enumerate(num_to_cnt):
+            acc_cnt += cnt
+            if acc_cnt >= mid_cnt:
+                mid_key = num
+                break  
+
+        remain_cnt = acc_cnt - mid_cnt
+        num_to_cnt[mid_key] -= remain_cnt
+
+        arr1 = deque()
+        for i in range(mid_key+1):
+            arr1.extendleft([i] * num_to_cnt[i])
         
+        arr2 = deque([mid_key] * remain_cnt)
+        for j in range(mid_key+1, 5001):
+            arr2.extendleft([j] * num_to_cnt[j])
+        
+        k = 0
+        for num1, num2 in zip(arr1, arr2):
+            nums[k] = num1
+            nums[k+1] = num2
+            k += 2
+        
+        if len(nums)%2 == 1:
+            nums[-1] = arr1[-1]
