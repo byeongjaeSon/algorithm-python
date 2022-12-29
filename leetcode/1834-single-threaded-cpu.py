d1996295b947eefb51b2b504104b356c1b24fb9c
손병재 <byeongjae.dev@gmail.com>
2022-12-29 23:56:52 +0900

    Refactor: Modify the solution to '1834-single-threaded-cpu' on LeetCode

--- leetcode/1834-single-threaded-cpu.py
+++ leetcode/1834-single-threaded-cpu.py
@@ -1,22 +1,22 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        cpu_tasks = [(idx, task[0], task[1]) for idx, task in enumerate(tasks)]
+        cpu_tasks = [(idx, enqueueTime, processingTime) for idx, (enqueueTime, processingTime) in enumerate(tasks)]
         cpu_tasks.sort(key = lambda x: -x[1])
         order = []
         
         curr_time = 0
         scheduler = []
-        while len(cpu_tasks) > 0 or len(scheduler) > 0:
-            while len(cpu_tasks) > 0 and cpu_tasks[-1][1] <= curr_time:
-                task = cpu_tasks.pop()
-                heapq.heappush(scheduler, (task[2], task[0]))
+        while cpu_tasks or scheduler:
+            while cpu_tasks and cpu_tasks[-1][1] <= curr_time:
+                (idx, _, processingTime) = cpu_tasks.pop()
+                heapq.heappush(scheduler, (processingTime, idx))
             
-            if len(scheduler) > 0:
-                processing_task = heapq.heappop(scheduler)
-                curr_time += processing_task[0]
-                order.append(processing_task[1])
+            if scheduler:
+                (processingTime, idx) = heapq.heappop(scheduler)
+                curr_time += processingTime
+                order.append(idx)
             else:
-                if len(cpu_tasks) > 0:
+                if cpu_tasks:
                     curr_time = cpu_tasks[-1][1]
         
         return order
