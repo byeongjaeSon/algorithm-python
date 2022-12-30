f925ce5d4b5fdeb4ac8dc35af08d46e3da524ec5
손병재 <byeongjae.dev@gmail.com>
2022-12-30 23:06:54 +0900

    Refactor: Modify the solution to '1-two-sum' on LeetCode

--- leetcode/1-two-sum.py
+++ leetcode/1-two-sum.py
@@ -1,16 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        n = len(nums)
-        val_to_idx = defaultdict(list)
-        for i in range(n):
-            val_to_idx[nums[i]].append(i)
-
-        numbers = set(nums)
-        for i in range(n):
-            remain = target-nums[i]
-            if remain in numbers:
-                if nums[i] == remain:
-                    if len(val_to_idx[remain]) > 1:
-                        return val_to_idx[remain][:2]
-                else:
-                    return [i, val_to_idx[remain][0]]
+        hashmap = {}
+        for i in range(len(nums)):
+            hashmap[nums[i]] = i
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in hashmap and hashmap[complement] != i:
+                return [i, hashmap[complement]] 
