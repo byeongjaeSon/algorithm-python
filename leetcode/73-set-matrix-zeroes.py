d15fdedfe053534ca19d98f6189a502c0ae8ef3b
손병재 <byeongjae.dev@gmail.com>
2022-12-30 21:51:31 +0900

    Refactor: Modify the solution to '73-set-matrix-zeroes' on LeetCode

--- leetcode/73-set-matrix-zeroes.py
+++ leetcode/73-set-matrix-zeroes.py
@@ -1,19 +1,18 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         m, n = len(matrix), len(matrix[0])
-        rows = set()
-        cols = set()
         for i in range(m):
             for j in range(n):
                 if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
-        
-        for row in rows:
-            for j in range(n):
-                matrix[row][j] = 0
-        
-        for col in cols:
-            for i in range(m):
-                matrix[i][col] = 0
+                    for k in range(m):
+                        if matrix[k][j] != 0:
+                            matrix[k][j] = None
 
+                    for k in range(n):
+                        if matrix[i][k] != 0:
+                            matrix[i][k] = None
+        
+        for i in range(m):
+            for j in range(n):
+                if matrix[i][j] == None:
+                    matrix[i][j] = 0
