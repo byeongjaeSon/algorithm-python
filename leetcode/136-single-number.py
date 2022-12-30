b55838dfc48eba9f34064ee332e12ea20c1b63e2
손병재 <byeongjae.dev@gmail.com>
2022-12-30 23:58:20 +0900

    Feat: Add the solution to '136-single-number' on LeetCode

--- leetcode/136-single-number.py
+++ leetcode/136-single-number.py
@@ -1,6 +1,3 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        num_to_cnt = Counter(nums)
-        for k, v in num_to_cnt.items():
-            if v == 1:
-                return k
+        return reduce(lambda acc, num : acc^num, nums)
