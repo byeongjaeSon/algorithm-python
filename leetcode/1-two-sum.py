3e6e622a7f3db74eb26133e230dcb97df2a9851f
손병재 <byeongjae.dev@gmail.com>
2022-12-30 23:00:46 +0900

    Refactor: Modify the solution to '1-two-sum' on LeetCode

--- leetcode/1-two-sum.py
+++ leetcode/1-two-sum.py
@@ -1,6 +1,20 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
+        def binary_search(lo, hi, target):
+            while lo < hi:
+                mid = (lo+hi)//2
+                if numbers[mid][0] > target:
+                    hi = mid
+                elif numbers[mid][0] < target:
+                    lo = mid+1
+                else:
+                    return numbers[mid][1] 
+            return None
+
+        n = len(nums)
+        numbers = [(nums[i], i) for i in range(n)]
+        numbers.sort(key = lambda x : x[0])
+        for i in range(n):
+            j = binary_search(0, n, target-nums[i])
+            if j != None and i != j:
+                return [i, j] 
