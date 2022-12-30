2c081882382ea71f8e46bf9c33e00ff8df64f602
손병재 <byeongjae.dev@gmail.com>
2022-12-30 15:11:48 +0900

    Refactor: Modify the solution to '797-all-paths-from-source-to-target' on LeetCode

--- leetcode/797-all-paths-from-source-to-target.py
+++ leetcode/797-all-paths-from-source-to-target.py
@@ -1,14 +1,16 @@
 class Solution:
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
-        ret = []
-        n = len(graph)-1
-        def dfs(curr_node, adjacent_nodes, path):
-            new_path = path.copy()
-            new_path.append(curr_node)
-            if curr_node == n:
-                ret.append(new_path)
-            for next_node in adjacent_nodes:
-                dfs(next_node, graph[next_node], new_path)
-
-        dfs(0, graph[0], [])
-        return ret
+        all_path = []
+        stack = [(0, [0])]
+        dest = len(graph)-1
+        
+        while stack:
+            curr_node, path = stack.pop()
+            if curr_node == dest:
+                all_path.append(path)
+                continue
+            
+            for next_node in graph[curr_node]:
+                stack.append((next_node, path+[next_node]))
+                
+        return all_path
