f8d30fe2f60385147c19d01a92a3db58bdc2e99d
손병재 <byeongjae.dev@gmail.com>
2023-01-08 17:51:56 +0900

    Refactor: Modify the solution to '149-max-points-on-a-line' on LeetCode

--- leetcode/Hard/149-max-points-on-a-line.py
+++ leetcode/Hard/149-max-points-on-a-line.py
@@ -1,17 +1,22 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
-        if len(points) == 1:
+        n = len(points)
+        if n == 1:
             return 1
             
         max_cnt = 0
-        for i in range(len(points)):
-            for j in range(i+1, len(points)):
+        for i in range(n):
+            mem = defaultdict(int)
+            for j in range(n):
+                if i == j: continue
                 (x1, y1), (x2, y2) = points[i], points[j]
-                cnt = 0
-                for point in points:
-                    (x, y) = point
-                    if (y - y1) * (x2 - x1) == (y2 - y1) * (x - x1):
-                        cnt += 1
-                max_cnt = max(max_cnt, cnt)
 
+                gradient = None
+                if x1 == x2:
+                    gradient = float("inf")
+                else:
+                    gradient = (y2 - y1) / (x2 - x1)
+                mem[gradient] += 1
+
+            max_cnt = max(max_cnt, max(mem.values())+1)
         return max_cnt
