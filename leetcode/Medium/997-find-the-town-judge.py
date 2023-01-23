23a12c2c9e551b24863f9c765d7782a9c8d9847a
손병재 <byeongjae.dev@gmail.com>
2023-01-23 19:29:00 +0900

    Modify: Refactor the solution to '997-find-the-town-judge' on LeetCode

--- leetcode/Medium/997-find-the-town-judge.py
+++ leetcode/Medium/997-find-the-town-judge.py
@@ -1,11 +1,12 @@
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:    
-        degree = [0] * (n+1)
+        out_degree = [0] * (n+1)
+        in_degree = [0] * (n+1)
         for a, b in trust:
-            degree[a] -= 1 # out-degree
-            degree[b] += 1 # in-degree
+            out_degree[a] += 1
+            in_degree[b] += 1
         
         for i in range(1, n+1):
-            if degree[i] == n-1:
+            if out_degree[i] == 0 and in_degree[i] == n-1:
                 return i
         return -1
