61ea1ae0027a2b1b813eaed7f1d17a64378b7c93
손병재 <byeongjae.dev@gmail.com>
2023-01-29 20:09:03 +0900

    Refactor: Modify the solution to '표현 가능한 이진트리' on Programmers

--- programmers/Level3/표현 가능한 이진트리.py
+++ programmers/Level3/표현 가능한 이진트리.py
@@ -1,38 +1,27 @@
+from math import log2
+
+def search(start, end, parent, tree):
+    mid = (start + end) // 2
+    curr = tree[mid]
+    if parent == "0" and curr == "1":
+        return False
+    if start == end:
+        return True
+    left = search(start, mid - 1, curr, tree)
+    right = search(mid + 1, end, curr, tree)
+    if left and right:
+        return True
+    else:
+        return False
+
 def solution(numbers):
-    def make_full_binary_number(number):
-        reverseBinaryNumber = []
-        while number != 1:
-            reverseBinaryNumber.append(str(number % 2))
-            number //= 2
-        reverseBinaryNumber.append("1")
-        binaryNumber = ''.join(reverseBinaryNumber[::-1])
-        binaryTreeSize = 1
-        while binaryTreeSize < len(binaryNumber):
-            binaryTreeSize = (binaryTreeSize + 1) * 2 - 1
-        binaryNumber = "0" * (binaryTreeSize - len(binaryNumber)) + binaryNumber
-        return binaryNumber
-    
-        
-    def is_valid(start, end, binary_number):
-        if start == end:
-            return binary_number[start]
-        
-        mid = (start + end) // 2
-        root_node = binary_number[mid]
-        left_subtree = is_valid(start, mid-1, binary_number)
-        if not left_subtree or (root_node == '0' and left_subtree == '1'):
-            return False
-        
-        right_subtree = is_valid(mid+1, end, binary_number)
-        if not right_subtree or (root_node == '0' and right_subtree == '1'):
-            return False
-        
-        if root_node == '0' and left_subtree == '0' and right_subtree == '0':
-            return '0'
-        return '1'
-    
     answer = []
-    for num in numbers:
-        binary_number = make_full_binary_number(num)
-        answer.append(int(is_valid(0, len(binary_number)-1, binary_number)))
-    return answer
+    for n in numbers:
+        b = bin(n)[2:]
+        b = b.zfill(2 ** (int(log2(len(b))) + 1) - 1)
+        if search(0, len(b) - 1, b[len(b) // 2], b):
+            answer.append(1)
+        else:
+            answer.append(0)
+
+    return answer   
