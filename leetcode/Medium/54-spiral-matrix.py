c3947772c5c85539ec1290ff8bad56756787a3b4
손병재 <byeongjae.dev@gmail.com>
2023-02-12 15:40:03 +0900

    Modify: Refactor the solution to '54-spiral-matrix' on Leetcode

--- leetcode/Medium/54-spiral-matrix.py
+++ leetcode/Medium/54-spiral-matrix.py
@@ -1,17 +1,10 @@
-class Solution(object):
-    def spiralOrder(self, matrix):
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
         result = []
+        matrix = deque([deque(m) for m in matrix])
         while matrix:
-            result += matrix.pop(0) 
-
-            if matrix and matrix[0]:  
-                for line in matrix:
-                    result.append(line.pop())
-
-            if matrix: 
-                result += matrix.pop()[::-1]
-
-            if matrix and matrix[0]: 
-                for line in matrix[::-1]:
-                    result.append(line.pop(0))
+            if matrix: result.extend(matrix.popleft())
+            result.extend([row.pop() for row in matrix if row])
+            if matrix: result.extend(reversed(matrix.pop()))
+            result.extend(reversed([row.popleft() for row in matrix if row]))
         return result
