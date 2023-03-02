d452e19b9ba25f8f22f0abb73ea095f1d417641d
손병재 <byeongjae.dev@gmail.com>
2023-03-03 01:04:45 +0900

    투 포인터
    O(n) / O(1)

--- leetcode/Medium/443-string-compression.py
+++ leetcode/Medium/443-string-compression.py
@@ -1,23 +1,20 @@
 class Solution:
     def compress(self, chars: List[str]) -> int:
         n = len(chars)
-        char_cnt = 1
-        for i in range(1, n):
-            if chars[i-1] != chars[i]:                
-                chars.append(chars[i-1])
-                if char_cnt > 1:
-                    for c in str(char_cnt):
-                        chars.append(c)
-                    char_cnt = 1
-            else:
-                char_cnt += 1
-                
-        chars.append(chars[n-1])
-        if char_cnt > 1:
-            for c in str(char_cnt):
-                chars.append(c)
-       
-        for _ in range(n):
-            chars.pop(0)
-
-        return len(chars)
+        i = j = 0
+        while i < n:
+            count = 1
+            while i < n - 1 and chars[i] == chars[i+1]:
+                count += 1
+                i += 1
+            
+            chars[j] = chars[i]
+            j += 1
+            
+            if count > 1:
+                for c in str(count):
+                    chars[j] = c
+                    j += 1
+            
+            i += 1
+        return j
