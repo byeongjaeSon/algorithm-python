89903fad65d3bbfae1ac4a58ca7f71ed5521ff71
손병재 <byeongjae.dev@gmail.com>
2023-03-05 23:50:47 +0900

    BFS
    O(n) / O(n)

--- leetcode/Hard/1345-jump-game-iv.py
+++ leetcode/Hard/1345-jump-game-iv.py
@@ -1,35 +1,36 @@
 class Solution:
     def minJumps(self, arr: List[int]) -> int:
         n = len(arr)
-        int_to_positions = defaultdict(list)
+        num_to_positions = defaultdict(list)
         for i, num in enumerate(arr):
-            int_to_positions[num].append(i)
-        
-        adj_list = defaultdict(set)
-        for i, num in enumerate(arr):
-            for pos in int_to_positions[num]:
-                adj_list[i].add(pos)
-            
-            if i-1 >= 0:
-                adj_list[i].add(i-1)
-            if i+1 < len(arr):
-                adj_list[i].add(i+1)
-
-        def bfs(adj_list, destination):
+            num_to_positions[num].append(i)
+       
+        def bfs():
             q = deque([0])
             level = 0
-            
+            visited = [False] * n
+            visited[0] = True
+
             while q:
                 q_size = len(q)
                 for _ in range(q_size):
                     curr = q.popleft()
-                    if curr == destination:
+                    if curr == n-1:
                         return level
-                    
-                    for next in adj_list[curr]:
-                        q.append(next)
-                level += 1
 
+                    for next in num_to_positions[arr[curr]]:
+                        if not visited[next]:
+                            visited[next] = True
+                            q.append(next)
+
+                    num_to_positions[arr[curr]].clear()
+
+                    for next in [curr-1 ,curr+1]:
+                        if 0 <= next < n and not visited[next]:
+                            visited[next] = True
+                            q.append(next)
+                 
+                level += 1
             return -1
         
-        return bfs(adj_list, n-1)
+        return bfs()
