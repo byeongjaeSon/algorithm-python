565768216ab146108728b6fb6cb83ac218b0ae45
손병재 <byeongjae.dev@gmail.com>
2023-03-06 19:55:38 +0900

    이진 탐색
    O(log n)/O(1)

--- leetcode/Easy/1339-kth-missing-positive-number.py
+++ leetcode/Easy/1339-kth-missing-positive-number.py
@@ -1,13 +1,11 @@
 class Solution:
     def findKthPositive(self, arr: List[int], k: int) -> int:
-        i, j = 0, 0
-        for num in range(1, 2001):
-            if i < len(arr) and num == arr[i]:
-                i += 1
-                continue
+        l, r = 0, len(arr)
+        while l < r:
+            mid = (l+r)//2
+            if arr[mid] - mid - 1 < k:
+                l = mid+1
             else:
-                j += 1
-            
-            if j == k:
-                return num
+                r = mid
         
+        return l + k
