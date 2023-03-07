0f7ff034b6c820d5be56dc89ec454ed6afaf9d48
손병재 <byeongjae.dev@gmail.com>
2023-03-07 23:54:44 +0900

    이진 탐색
    O(n*log(m*k)) / O(1)

--- leetcode/Medium/2187-minimum-time-to-complete-trips.py
+++ leetcode/Medium/2187-minimum-time-to-complete-trips.py
@@ -1,10 +1,16 @@
 class Solution:
     def minimumTime(self, time: List[int], totalTrips: int) -> int:
-        curr_time = 0
-        total_trip_cnts = 0
-        while total_trip_cnts < totalTrips:
-            curr_time += 1
+        def need_more_time(curr_time):
+            total_trip_cnts = 0
             for t in time:
-                if curr_time % t == 0:
-                    total_trip_cnts += 1
-        return curr_time
+                total_trip_cnts += curr_time // t
+            return total_trip_cnts < totalTrips
+
+        l, r = 1, min(time) * totalTrips #[l, r]
+        while l < r:
+            mid = (l+r)//2
+            if need_more_time(mid):
+                l = mid + 1
+            else:
+                r = mid
+        return l
